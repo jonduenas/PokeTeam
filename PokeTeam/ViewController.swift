235cc87ec62c1b213d16db27fc377631f266853a
Jon Duenas <jonduenas@icloud.com>
2020-07-31 16:28:32 -0700

    Initial Commit

---
+++ PokeTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeTeam
+//
+//  Created by Jon Duenas on 7/31/20.
+//  Copyright © 2020 Jon Duenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
