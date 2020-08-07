6cc5429f3aa1c56790f26fe95a886da0adc2172c
Jon Duenas <jonduenas@icloud.com>
2020-08-07 15:16:47 -0700

    Update PokemonDetailVC with data from API.

--- PokeTeam/Controllers/PokemonDetailVC.swift
+++ PokeTeam/Controllers/PokemonDetailVC.swift
@@ -19,7 +19,7 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet var pokemonNameLabel: UILabel!
     @IBOutlet var pokemonType1: UILabel!
     @IBOutlet var pokemonType2: UILabel!
-    @IBOutlet var pokemonNumberLabel: UILabel!
+    @IBOutlet var pokemonNumberAndGenusLabel: UILabel!
     @IBOutlet var pokemonDescriptionLabel: UILabel!
     
     @IBOutlet var baseStatsHeaderLabel: UILabel!
@@ -58,7 +58,65 @@ class PokemonDetailVC: UIViewController {
     }
     
     private func loadPokemonInfo() {
+        let pokemonIndex = pokemon.entryNumber
+
+        PokemonManager.shared.fetchFromAPI(index: pokemonIndex, dataType: .pokemon, decodeTo: PokemonData.self) { (pokemon) in
+            PokemonManager.shared.fetchFromAPI(index: pokemonIndex, dataType: .species, decodeTo: SpeciesData.self) { (species) in
+                DispatchQueue.main.async {
+                    self.updatePokemonUI(with: pokemon, species: species)
+                }
+            }
+        }
+    }
+    
+    private func updatePokemonUI(with pokemon: PokemonData, species: SpeciesData) {
+        // Update Pokemon Name
+        pokemonNameLabel.text = pokemon.name.capitalized
         
+        // Update Pokemon types
+        if pokemon.types.count > 1 {
+            let type1 = pokemon.types[0].name
+            let type2 = pokemon.types[1].name
+            
+            pokemonType1.text = type1.capitalized
+            pokemonType1.backgroundColor = PokemonManager.shared.colorDictionary[PokemonType(rawValue: type1)!]
+            print(PokemonType(rawValue: type1)!)
+            pokemonType2.text = type2.capitalized
+            pokemonType2.backgroundColor = PokemonManager.shared.colorDictionary[PokemonType(rawValue: type2)!]
+        } else {
+            let type1 = pokemon.types[0].name
+            pokemonType1.text = type1.capitalized
+            pokemonType1.backgroundColor = PokemonManager.shared.colorDictionary[PokemonType(rawValue: type1)!]
+            pokemonType2.isHidden = true
+        }
+        
+        // Update Pokemon ID# and Genus text
+        let pokemonIDString = String(withInt: pokemon.id, leadingZeros: 3)
+        
+        var genusText = ""
+        for genus in species.genera {
+            if genus.language == "en" {
+                genusText = genus.genus
+            }
+        }
+        
+        pokemonNumberAndGenusLabel.text = "#\(pokemonIDString) –– \(genusText)"
+        
+        // Update Pokemon Description
+        var englishFlavorTextArray = [String]()
+        var englishFlavorText = ""
+        
+        for entry in species.flavorTextEntries {
+            if entry.language == "en" {
+                englishFlavorTextArray.append(entry.flavorText)
+                if let latestEntry = englishFlavorTextArray.last {
+                   englishFlavorText = latestEntry
+                }
+            }
+        }
+        let formattedText = englishFlavorText.replacingOccurrences(of: "\n", with: " ")
+        
+        pokemonDescriptionLabel.text = formattedText
     }
     
     private func setCustomFonts() {
@@ -88,4 +146,18 @@ class PokemonDetailVC: UIViewController {
     }
     */
 
+}
+
+extension String {
+    init(withInt int: Int, leadingZeros: Int = 1) {
+        self.init(format: "%0\(leadingZeros)d", int)
+    }
+
+    func leadingZeros(_ zeros: Int) -> String {
+        if let int = Int(self) {
+            return String(withInt: int, leadingZeros: zeros)
+        }
+        print("Warning: \(self) is not an Int")
+        return ""
+    }
 }
