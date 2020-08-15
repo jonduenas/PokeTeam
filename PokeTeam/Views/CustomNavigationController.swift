f7e677f29e5b7ff3215d63d44e18cdae89f61fbc
Jon Duenas <jonduenas@icloud.com>
2020-08-14 18:05:35 -0700

    Override default status bar style to match navigation bar text color.

---
+++ PokeTeam/Views/CustomNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationController.swift
+//  PokeTeam
+//
+//  Created by Jon Duenas on 8/14/20.
+//  Copyright © 2020 Jon Duenas. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+}
