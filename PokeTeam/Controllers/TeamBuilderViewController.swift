e30cc82803c0ff3bec23a3d70325b255735f9474
Jon Duenas <jonduenas@icloud.com>
2020-09-14 12:53:23 -0700

    Convert TeamBuilder from UICollectionViewController to UIViewController with embedded UICollectionView.

---
+++ PokeTeam/Controllers/TeamBuilderViewController.swift
@@ -0,0 +1,103 @@
+//
+//  TeamBuilderViewController.swift
+//  PokeTeam
+//
+//  Created by Jon Duenas on 9/14/20.
+//  Copyright © 2020 Jon Duenas. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+private let reuseIdentifier = "PokemonCollectionCell"
+private let segueIdentifier = "detailSegue"
+
+class TeamBuilderViewController: UIViewController {
+
+    let simpleOver = SimpleOver()
+    let testArray = ["Pokemon 1", "Pokemon 2", "Pokemon 3"]
+    var teamsArray = [TeamMO]()
+    var team = [PokemonMO]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.delegate = self
+        navigationItem.title = "TEAM BUILDER"
+        navigationController?.navigationBar.setNavigationBarColor(to: UIColor(named: "team-builder"))
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadSavedTeam()
+    }
+
+    private func loadSavedTeam() {
+        print("attempting load of team")
+        
+        let request: NSFetchRequest<TeamMO> = TeamMO.fetchRequest()
+        
+        do {
+            teamsArray = try PokemonManager.shared.context.fetch(request)
+            if teamsArray.count > 0 {
+                print("Found \(teamsArray.count) stored teams")
+                let firstTeam = teamsArray[0]
+                let teamSet = firstTeam.members
+                team = teamSet?.array as! [PokemonMO]
+            }
+        } catch {
+            print("Team fetch failed: \(error)")
+        }
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath = collectionView.indexPathsForSelectedItems!
+        let selectedItem = indexPath[0].row
+        
+        if segue.identifier == segueIdentifier {
+            let detailVC = segue.destination as! PokemonBuilderVC
+            detailVC.pokemonName = team[selectedItem].name
+            detailVC.pokemonImageName = team[selectedItem].imageID
+            detailVC.pokemonManagedObjectID = team[selectedItem].objectID
+        }
+    }
+}
+
+extension TeamBuilderViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return team.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokemonCollectionCell
+        
+        // Configure the cell
+        cell.setPokemonInfo(for: team[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected \(indexPath.item)")
+    }
+}
+
+extension TeamBuilderViewController: UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        simpleOver.popStyle = (operation == .pop)
+        return simpleOver
+    }
+}
