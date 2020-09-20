3aa6de525fb7187a3ff991cc1cb537a1c6f31f62
Jon Duenas <jonduenas@icloud.com>
2020-09-20 14:29:41 -0700

    Add CoreDataStack.

---
+++ PokeTeam/Models/CoreDataStack.swift
@@ -0,0 +1,69 @@
+//
+//  CoreDataStack.swift
+//  PokeTeam
+//
+//  Created by Jon Duenas on 9/20/20.
+//  Copyright © 2020 Jon Duenas. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+open class CoreDataStack {
+    public static let modelName = "PokeTeam"
+    
+    public init() {}
+    
+    public lazy var mainContext: NSManagedObjectContext = {
+      return self.persistentContainer.viewContext
+    }()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.modelName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+            container.viewContext.automaticallyMergesChangesFromParent = true
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    public func newDerivedContext() -> NSManagedObjectContext {
+        let context = persistentContainer.newBackgroundContext()
+        return context
+    }
+    
+    public func saveContext() {
+        saveContext(mainContext)
+    }
+    
+    public func saveContext(_ context: NSManagedObjectContext) {
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    public func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+            self.saveContext(self.mainContext)
+        }
+    }
+}
