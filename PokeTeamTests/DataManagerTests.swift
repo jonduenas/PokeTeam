a751d0f9634c870c6eeb57bb3f48c4a355089fcc
Jon Duenas <jonduenas@icloud.com>
2020-10-11 15:02:58 -0700

    Add test for updateDetails

--- PokeTeamTests/DataManagerTests.swift
+++ PokeTeamTests/DataManagerTests.swift
@@ -12,28 +12,34 @@ import CoreData
 
 class DataManagerTests: XCTestCase {
 
-    var dataManager: DataManager!
+    var sut: DataManager!
     var coreDataStack: CoreDataStack!
     
-    override func setUpWithError() throws {
-        try super.setUpWithError()
+    override func setUp() {
+        super.setUp()
         coreDataStack = TestCoreDataStack()
-        dataManager = DataManager(managedObjectContext: coreDataStack.mainContext, coreDataStack: coreDataStack)
+        sut = DataManager(managedObjectContext: coreDataStack.mainContext, coreDataStack: coreDataStack)
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         coreDataStack = nil
-        dataManager = nil
-        try super.tearDownWithError()
+        sut = nil
+        super.tearDown()
     }
     
     func testAddPokemon() {
-        let pokemon = dataManager.addPokemon(name: "pikachu", speciesURL: "http://testurl.com", id: 4)
+        let pokemon = sut.addPokemon(name: "pikachu", speciesURL: "http://testurl.com", id: 4)
         
         XCTAssertNotNil(pokemon, "Pokemon should not be nil")
         XCTAssertTrue(pokemon.name == "pikachu")
         XCTAssertTrue(pokemon.speciesURL == "http://testurl.com")
         XCTAssertTrue(pokemon.id == 4)
+    }
+    
+    func testAddTeam() {
+        let newTeam = sut.addTeam()
+        
+        XCTAssertNotNil(newTeam)
     }
 
     func testUpdatePokedex() {
@@ -46,10 +52,44 @@ class DataManagerTests: XCTestCase {
         let testPokedex = NationalPokedex(count: 4, results: [pokemon1, pokemon2, pokemon3, pokemon4])
         
         // when
-        let testPokemonArray = dataManager.updatePokedex(pokedex: testPokedex)
+        let testPokemonArray = sut.updatePokedex(pokedex: testPokedex)
         
         // then
         XCTAssertEqual(testPokemonArray.count, 4, "testPokemonArray count should be 4")
         XCTAssertEqual(testPokemonArray[1].id, 2, "ID should be 2")
+    }
+    
+    func testUpdateDetails() {
+        // Create test Pokemon MO
+        let pokemon = sut.addPokemon(name: "pikachu", speciesURL: "http://testurl.com", id: 25)
+        let managedObjectID = pokemon.objectID
+        
+        // Inject test JSON file and decode to SpeciesData
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "pikachu", withExtension: "json") else {
+            XCTFail("Missing pikachu.json file")
+            return
+        }
+        
+        let testJSON = try? Data(contentsOf: url)
+        
+        let speciesData = try? jsonDecoder.decode(SpeciesData.self, from: testJSON!)
+        
+        // Test updateDetails(for:with:)
+        let updatedPokemon = sut.updateDetails(for: managedObjectID, with: speciesData!)
+        
+        XCTAssertNotNil(updatedPokemon)
+        XCTAssertNotNil(updatedPokemon.genus)
+        XCTAssertNotNil(updatedPokemon.generation)
+        XCTAssertNotNil(updatedPokemon.flavorText)
+        XCTAssertNotNil(updatedPokemon.isBaby)
+        XCTAssertNotNil(updatedPokemon.isLegendary)
+        XCTAssertNotNil(updatedPokemon.isMythical)
+        XCTAssertNotNil(updatedPokemon.nationalPokedexNumber)
+        XCTAssertNotNil(updatedPokemon.order)
+        XCTAssertNotNil(updatedPokemon.pokemonURL)
     }
 }
